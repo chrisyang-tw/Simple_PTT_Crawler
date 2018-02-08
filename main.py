46f2900adda169c1cc652046c1b8716c713fcb55
chrisyang1224 <christopheryang2012@gmail.com>
2018-02-09 05:09:11 +0800

    version 3

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ from bs4 import BeautifulSoup
 from pretty_print import pretty_print ## 美化輸出
 import urllib.parse ## url 相關應用
 
-index = str(input('請輸入ptt看板網址 (ex: https://www.ptt.cc/bbs/movie/index.html)：\n'))
-pages = eval(input('請輸入想爬的頁數 ex: 5：'))
+index = str(input('想抓取哪個ptt看板？(ex: Movie版請輸入 https://www.ptt.cc/bbs/movie/index.html)：\n'))
+pages = eval(input('想抓取幾頁呢？ex: 5：'))
 
 not_exist = BeautifulSoup('<a>(本文已被刪除)</a>', 'lxml').a ## '本文已被刪除'的結構不同，自行生成<a>
 
-def get_articles_on_ptt(url):
+def get_articles_on_ptt(url): ## 爬取一頁的文章
     response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml') ## 網頁原始碼
+    soup = BeautifulSoup(response.text, 'lxml') ## 得到網頁原始碼
     articles = []
 
     for i in soup.find_all('div', 'r-ent'):
-        meta = i.find('div', 'title').find('a') or not_exist ## 當爬到是空的，讓 meta = 0
+        meta = i.find('div', 'title').find('a') or not_exist
         articles.append({
             'title': meta.getText().strip(), ## strip 去除頭尾字符，預設是空白
             'push': i.find('div', 'nrec').getText(),
@@ -39,18 +39,19 @@ def get_pages(num): ## 要爬幾頁
 
 data = get_pages(pages)
 
-# for k in data:
-#     pretty_print(k['push'], k['title'], k['date'], k['author'])
+for k in data: ## 輸出至螢幕
+    pretty_print(k['push'], k['title'], k['date'], k['author'])
 
 csv_or_not = input('輸入 y 以匯出成csv檔，輸入其他結束程式：')
 
 if csv_or_not == 'y':
-    board = index.split('/')[-2]
-    csv = open('./ptt_%s版_前%d頁.csv'%(board, pages), 'a+', encoding='utf-8')
+    board = index.split('/')[-2] ## 取出看板名
+    csv = open('./ptt_%s版_前%d頁.csv'%(board, pages), 'a+', encoding='utf-8') ## 檔名格式，'a+'代表可覆寫
     csv.write('推文數,標題,發文日期,作者ID,\n')
     for l in data:
         l['title'] = l['title'].replace(',', '，') ## 與用來分隔的逗點作區別
         csv.write(l['push'] + ',' + l['title'] + ',' + l['date'] + ',' + l['author'] + ',\n')
     csv.close()
+    print('csv檔案已儲存在您的資料夾中。')
 else:
     quit()
